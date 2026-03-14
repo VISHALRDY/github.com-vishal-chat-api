2501813ec3ffa2276c09cb8a57b1755b793a2014
Vishal Reddy <vishalreddy9704@gmail.com>
2026-03-14 18:26:31 -0500

    Fix SQLite startup crash on Azure

--- Program.cs
+++ Program.cs
@@ -98,7 +98,7 @@ app.MapHub<ChatHub>("/chatHub");
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-    db.Database.Migrate();
+db.Database.EnsureCreated();
 }
 
 app.Run();
