c57e6c009e76998d7b3414ba3fbd0bfad80f6d8f
Vishal Reddy <vishalreddy9704@gmail.com>
2026-03-15 17:26:10 -0500

    fixed program.cs

--- Program.cs
+++ Program.cs
@@ -79,6 +79,11 @@ builder.Services.AddCors(options =>
 });
 
 var app = builder.Build();
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.EnsureCreated();
+}
 
 // Swagger
 app.UseSwagger();
